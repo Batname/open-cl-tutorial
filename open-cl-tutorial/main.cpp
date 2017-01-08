fc508155e6861fe1a36ae5efb7f6630c5fd339e3
DenisDubinin <dadubinin@gmail.com>
2017-01-08 19:36:34 +0100

    add device support log

--- open-cl-tutorial/main.cpp
+++ open-cl-tutorial/main.cpp
@@ -1,15 +1,38 @@
-//
-//  main.cpp
-//  open-cl-tutorial
-//
-//  Created by apple on 2017-01-08.
-//  Copyright © 2017 apple. All rights reserved.
-//
-
 #include <iostream>
+#include <vector>
+#include <cassert>
+
+#include <OpenCL/OpenCL.h>
+
+// #include "first.cl.h"
+// or upload file
+
+#define LOG_SIZE 128
+
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    cl_uint platformIdCount;
+    cl_uint deviceIdCount = 0;
+    char logInfoBuf[LOG_SIZE];
+    
+    clGetPlatformIDs(1, nullptr, &platformIdCount);
+    vector<cl_platform_id> platformIds(platformIdCount);
+    clGetPlatformIDs(platformIdCount, platformIds.data(), nullptr);
+    assert(platformIds.size() > 0);
+    
+    clGetDeviceIDs(platformIds[0], CL_DEVICE_TYPE_ALL, 0, nullptr, &deviceIdCount);
+    vector<cl_device_id> devicesIds(platformIdCount);
+    clGetDeviceIDs(platformIds[0], CL_DEVICE_TYPE_ALL, deviceIdCount, devicesIds.data(), nullptr);
+    assert(devicesIds.size() > 0);
+    
+    for(auto &device : devicesIds) {
+        clGetDeviceInfo(device, CL_DEVICE_VENDOR, LOG_SIZE, logInfoBuf, NULL);
+        cout << logInfoBuf << endl;
+        clGetDeviceInfo(device, CL_DEVICE_VERSION, LOG_SIZE, logInfoBuf, NULL);
+        cout << logInfoBuf << endl;
+    }
+
     return 0;
 }
